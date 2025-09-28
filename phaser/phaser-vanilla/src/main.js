92715a72e18f4b155e4211f963791926be46afa8
b-rucel <b-rucel@users.noreply.github.com>
2025-09-27 23:51:54 -0700

    adding phaser and config to page, blank canvas right now

--- phaser/phaser-vanilla/src/main.js
+++ phaser/phaser-vanilla/src/main.js
@@ -1,24 +1,20 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { Game, WEBGL } from 'phaser';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const config = {
+  type: WEBGL,
+  parent: 'app',
+  width: 800,
+  height: 600,
+  backgroundColor: '#1DA1F2',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
+}
 
-setupCounter(document.querySelector('#counter'))
+// eslint-disable-next-line no-new
+new Game(config);
