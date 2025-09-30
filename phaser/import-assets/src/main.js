6a5d946a2d5096eae095fae25a1f57c65dea9260
b-rucel <b-rucel@users.noreply.github.com>
2025-09-30 16:51:29 -0700

    adding simple sprite example, movement left and right

--- phaser/import-assets/src/main.js
+++ phaser/import-assets/src/main.js
@@ -2,6 +2,8 @@ import './style.css'
 import { Scene, Game, WEBGL } from 'phaser'
 import background from './alice.webp'
 
+import dude from './dude.sprite.png'
+
 const canvas = document.getElementById('game');
 
 class GameScene extends Scene {
@@ -11,6 +13,10 @@ class GameScene extends Scene {
 
   preload() {
     this.load.image('background', background)
+
+    this.load.spritesheet('dude', dude, {
+      frameWidth: 32, frameHeight: 48
+    })
   }
 
   create() {
@@ -47,6 +53,54 @@ class GameScene extends Scene {
       backgroundColor: 'rgba(0,0,0,0.7)',
       padding: { x: 8, y: 8 }
     })
+
+
+    this.player = this.physics.add.sprite(300, 250, 'dude')
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    // Create keyboard input
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.anims.play('left', true);
+    }
+    else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+      this.player.anims.play('right', true);
+    }
+    else {
+      this.player.setVelocityX(0);
+      this.player.anims.play('turn');
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-330);
+    }
   }
 }
 
@@ -55,6 +109,13 @@ const config = {
   width: 800,
   height: 600,
   canvas,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      // gravity: { y: 300 },
+      debug: false
+    }
+  },
   scene: [
     GameScene
   ]
